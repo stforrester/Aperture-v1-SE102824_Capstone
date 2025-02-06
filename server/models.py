bffbd1a118554a330f9029554b5771341bf42396
Stuart Forrester <stforrester@gmail.com>
2025-02-06 10:20:27 -0600

    updating models

--- server/models.py
+++ server/models.py
@@ -4,3 +4,28 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String)
+    lastname = db.Column(db.String)
+    username = db.Column(db.String, unique=True)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+class PhotoShoot(db.Model, SerializerMixin):
+    __tablename__ = 'photo_shoots'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    date = db.Column(db.DateTime)
+    description = db.Column(db.String)
+    photographer = db.Column(db.String)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+
