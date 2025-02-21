8799960bc94d01bb74a2ad718930cc4cab7221ea
Stuart Forrester <stforrester@gmail.com>
2025-02-21 04:50:08 -0600

    adds in NavBar_HomePage.js component as a child component of HomePage.js and updates HomePage.js to remove the logout button feature and have the NavBar_HomePage.js component take on this button and its functionality (including the updateUser prop passed down from App.js state to handle logouts

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,35 +1,14 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
+import NavBar_HomePage from './NavBar_HomePage.js'
+
 function HomePage({ updateUser, user }) {
-    const [error, setError] = useState(null)
-    const navigate = useNavigate()
-
-    const logout = () => {
-        fetch('/logout',
-            {
-                method:'DELETE',
-                headers: {
-                    "Content-Type":"application/json",
-                }
-            }
-        )
-        .then(response => {
-            if(response.ok){
-                updateUser(null)
-                navigate('/')
-            }
-            else {
-                response.json()
-                .then(error => setError(error))
-            }
-        })
-    }
-
+    
     return (
         <>
         <h1>Welcome, {user.firstname} {user.lastname}!</h1>
-        <button type="button" onClick={logout}>Logout</button>
+        <NavBar_HomePage updateUser={updateUser}/>
         </>
     )
 }
---
+++ client/src/components/NavBar_HomePage.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+
+function NavBar_HomePage({ updateUser }) {
+    const [error, setError] = useState(null)
+    const navigate = useNavigate()
+
+    const logout = () => {
+        fetch('/logout',
+            {
+                method:'DELETE',
+                headers: {
+                    "Content-Type":"application/json",
+                }
+            }
+        )
+        .then(response => {
+            if(response.ok){
+                updateUser(null)
+                navigate('/')
+            }
+            else {
+                response.json()
+                .then(error => setError(error))
+            }
+        })
+    }
+    
+    return (
+        <>
+        <span>
+            Menu:       
+            <button type="button">View Order History</button>
+            <button type="button">Edit Account Info</button>
+            <button type="button" onClick={logout}>Logout</button>
+        </span>
+        </>
+    )
+}
+
+export default NavBar_HomePage
