b2af216663ee3bd8f8640c4b88f1dd8d09ed0fe4
Stuart Forrester <stforrester@gmail.com>
2025-02-25 17:14:28 -0600

    updates PhotoShootContainer.js to include useEffect() for fetch to '/photo_shoots' to set state to the photoshoots in the database for the logged-in user

--- client/src/components/PhotoShootContainer.js
+++ client/src/components/PhotoShootContainer.js
@@ -5,6 +5,29 @@ import PhotoShootFilters from './PhotoShootFilters.js'
 
 function PhotoShootContainer() {
 
+    const [photoShootsData, setPhotoShootsData] = useState()
+    const [photoShoots, setPhotoShoots] = useState()
+    const [error, setError] = useState()
+
+    useEffect(() => {
+        fetch('/photo_shoots')
+        .then(response => {
+            if(response.ok){
+                response.json()
+                .then(data =>{
+                    setPhotoShoots(data)
+                    setPhotoShootsData(data)
+                })
+            }
+            else {
+                response.json()
+                .then(error => setError(error))
+            }
+        })
+    }, [setPhotoShootsData])
+
+    // const handleSortClick = ()
+
 }
 
 export default PhotoShootContainer
