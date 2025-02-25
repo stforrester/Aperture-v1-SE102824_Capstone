17bccb467bd05926a2de84141188250b54cc5f0d
Stuart Forrester <stforrester@gmail.com>
2025-02-25 16:26:46 -0600

    updates package.json to include bootstrap and react-bootstrap libraries, adds initial PhotoShootCard.js component

---
+++ client/src/components/PhotoShootCard.js
@@ -0,0 +1,9 @@
+import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup'
+import Badge from 'react-bootstrap/Badge'
+
+const PhotoShootCard = () => {
+
+}
+
+export default PhotoShootCard
