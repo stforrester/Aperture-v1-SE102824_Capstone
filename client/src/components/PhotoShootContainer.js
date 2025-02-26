bc7e7df3e919e057d4e24cc5b8697b4207fa524f
Stuart Forrester <stforrester@gmail.com>
2025-02-26 17:05:33 -0600

    updates PhotoShootContainer.js to have working callback functions that will re-set the photoshoots array state to be sorted in either ascending or descending order based on the property we are sorting by - passes these callback functions as props to the PhotoShootFilters child component

--- client/src/components/PhotoShootContainer.js
+++ client/src/components/PhotoShootContainer.js
@@ -26,11 +26,34 @@ function PhotoShootContainer() {
         })
     }, [setPhotoShootsData])
 
+    const handleSortClickAscending = (sort_field, nested=null) => {
+        const sorted_photoShoots = photoShoots.toSorted((a, b) => {
+            const a_sort = nested != null ? a[nested][sort_field] : a[sort_field]
+            const b_sort = nested != null ? b[nested][sort_field] : b[sort_field]
+            if (a_sort < b_sort) return -1;
+            if (a_sort > b_sort) return 1;
+            return 0; 
+          });
+        setPhotoShoots(sorted_photoShoots)
+    }
+
+    const handleSortClickDescending = (sort_field, nested=null) => {
+        const sorted_photoShoots = photoShoots.toSorted((a, b) => {
+            const a_sort = nested != null ? a[nested][sort_field] : a[sort_field]
+            const b_sort = nested != null ? b[nested][sort_field] : b[sort_field]
+            if (a_sort < b_sort) return 1;
+            if (a_sort > b_sort) return -1;
+            return 0; 
+          });
+        setPhotoShoots(sorted_photoShoots)
+    }
+
     if(!photoShoots) return <div>Loading Photo Shoots...</div>
 
     return (
         <div>
-            
+            <PhotoShootFilters handleSortAscendingSelected={handleSortClickAscending} handleSortDescendingSelected={handleSortClickDescending} />
+            <PhotoShootList photoShoots={photoShoots} />
         </div>
     )
 
