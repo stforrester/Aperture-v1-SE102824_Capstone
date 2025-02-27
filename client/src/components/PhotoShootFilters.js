18dcb5478fe6184f81349f9230ea0b4da98904e4
Stuart Forrester <stforrester@gmail.com>
2025-02-27 13:03:27 -0600

    updates PhotoShootFilters.js to include headers, sort buttons for 'date' and 'name' with corresponding sort drop downs for ascending or descending which will trigger the 'handleSortAscending/DescendingSelected' callback functions passed down as props from the parent component which will update state in that component accordingly

--- client/src/components/PhotoShootFilters.js
+++ client/src/components/PhotoShootFilters.js
@@ -2,9 +2,45 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Dropdown from 'react-bootstrap/Dropdown'
+import DropdownItem from 'react-bootstrap/esm/DropdownItem'
 
-function PhotoShootFilters() {
+function PhotoShootFilters({handleSortAscendingSelected, handleSortDescendingSelected}) {
+    return (
+        <Container fluid>
+            <Col xs={2}>
+                <span>My Photo Shoots:</span>
+            </Col>
 
+            <Col xs={5}></Col>
+
+            <Col xs={1}>Sort By:</Col>
+
+            <Col xs={1}>
+                <Dropdown>
+                    <Dropdown.Toggle>
+                        Date
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item onClick={()=>handleSortAscendingSelected('date')}>Ascending</Dropdown.Item>
+                        <Dropdown.Item onClick={()=>handleSortDescendingSelected('date')}>Descending</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </Col>
+
+            <Col xs={1}>
+                <Dropdown>
+                    <Dropdown.Toggle>
+                        Name
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        <Dropdown.Item onClick={()=>handleSortAscendingSelected('title')}>A - Z</Dropdown.Item>
+                        <Dropdown.Item onClick={()=>handleSortDescendingSelected('title')}>Z - A</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </Col>
+
+        </Container>
+    )
 }
 
 export default PhotoShootFilters
