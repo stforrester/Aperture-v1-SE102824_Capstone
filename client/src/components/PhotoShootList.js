5d7551c73311ca8302775e50e88ee63849191fd1
Stuart Forrester <stforrester@gmail.com>
2025-02-27 16:07:28 -0600

    updates PhotoShootList.js component to take in photoShoots as a prop from the PhotoShootContainer component's corresponding state value and then map each item of the photoShoots array to a PhotoShootCard.js child component

--- client/src/components/PhotoShootList.js
+++ client/src/components/PhotoShootList.js
@@ -5,8 +5,18 @@ import Col from 'react-bootstrap/Col'
 
 import PhotoShootCard from './PhotoShootCard.js'
 
-function PhotoShootList() {
-
+function PhotoShootList({photoShoots}) {
+    return (
+        <Container>
+            <Row>
+                {photoShoots && photoShoots.map((photoShoot) => 
+                    <Col key={photoShoot.id} xs={4}>
+                        <PhotoShootCard photoShoot={photoShoot} />
+                    </Col>
+                )}
+            </Row>
+        </Container>
+    )
 }
 
 export default PhotoShootList
