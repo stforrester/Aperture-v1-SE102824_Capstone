024a2d03b80a750ba9ed6f687b26f1b82b1f5236
Stuart Forrester <stforrester@gmail.com>
2025-02-28 19:54:01 -0600

    completes initial PhotoShootDetailView.js and NavBar_PhotoShoot.js components

---
+++ client/src/components/NavBar_PhotoShoot.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+function NavBar_PhotoShoot({ updateUser }) {
+    const [error, setError] = useState(null)
+    const navigate = useNavigate()
+
+    const logout = () => {
+        fetch('/logout',
+            {
+                method:'DELETE',
+                headers: {
+                    "Content-Type":"application/json",
+                }
+            }
+        )
+        .then(response => {
+            if(response.ok){
+                updateUser(null)
+                navigate('/')
+            }
+            else {
+                response.json()
+                .then(error => setError(error))
+            }
+        })
+    }
+
+    const handleHomePageRedirect = () => {
+        navigate("/")
+    }
+
+    const handleEditAccountRedirect = () => {
+        navigate("/edit_account")
+    }
+
+    return (
+        <>
+        <span>
+            Menu:
+            <button type="button" onClick={handleHomePageRedirect}>My Photo Shoots</button>
+            <button type="button">View Order History</button>
+            <button type="button" onClick={handleEditAccountRedirect}>Edit Account Info</button>
+            <button type="button" onClick={logout}>Logout</button>
+        </span>
+        </>
+    )
+}
+
+export default NavBar_PhotoShoot
---
+++ client/src/components/PhotoShootDetailView.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+
+import NavBar_PhotoShoot from './NavBar_PhotoShoot.js'
+import PhotosContainer from './PhotosContainer.js'
+
+function PhotoShootDetailView() {
+
+    const navigate = useNavigate()
+    const { id } = useParams()
+
+    const [photoShootData, setPhotoShootData] = useState()
+    const [photoShoot, setPhotoShoot] = useState()
+    const [error, setError] = useState()
+
+    useEffect(() => {
+        fetch(`photo_shoots/${id}`)
+        .then(response => {
+            if(response.ok){
+                response.json()
+                .then(data =>{
+                    setPhotoShoot(data)
+                    setPhotoShootData(data)
+                })
+            }
+            else {
+                response.json()
+                .then(error => setError(error))
+            }
+        })
+    }, [setPhotoShootData])
+    
+    return (
+        <>
+        <NavBar_PhotoShoot updateUser={updateUser}/>
+        <h2></h2>
+        <PhotosContainer />
+        </>
+    )
+}
+
+export default PhotoShootDetailView
