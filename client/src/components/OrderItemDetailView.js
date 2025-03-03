3672e7b3a14857e34cd9479f0f837b37d07dfbc2
Stuart Forrester <stforrester@gmail.com>
2025-03-03 16:33:54 -0600

    adds OrderItemDetailView.js component so that when a photo in the user's cart is clicked on for 'view photo' it displays a detailed view of the photo

---
+++ client/src/components/OrderItemDetailView.js
@@ -0,0 +1,32 @@
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Button from 'react-bootstrap/Button'
+import { Image } from 'cloudinary-react'
+
+function OrderItemDetailView({ orderItem, handleSetOrderItem }) {
+
+    const photo = orderItem.photo
+    
+    return (
+        <Container>
+            <Row>
+                <Image cloudName="stuart-forrester" publicId={photo.cloudinary_link} alt={`Photo ${photo.id} Image`} />
+            </Row>
+            <Row>
+                <Col xs={4}>
+                    <Button>Remove</Button>
+                </Col>
+                <Col xs={4}>
+                    ${photo.photo_price}
+                </Col>
+                <Col xs={4}>
+                    <Button onClick={()=>handleSetOrderItem(null)}>Return to Cart</Button>
+                </Col>
+            </Row>
+        </Container>
+    )
+
+}
+
+export default OrderItemDetailView
