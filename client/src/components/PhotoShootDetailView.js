feddf435561fd1a1643ee113e2237eb45a56f7f5
Stuart Forrester <stforrester@gmail.com>
2025-03-03 18:21:18 -0600

    adds CartButton.js to PhotoShootDetailView

--- client/src/components/PhotoShootDetailView.js
+++ client/src/components/PhotoShootDetailView.js
@@ -1,12 +1,27 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 import NavBar_PhotoShoot from './NavBar_PhotoShoot.js'
 import PhotosContainer from './PhotosContainer.js'
+import CartButton from './CartButton.js'
 
 function PhotoShootDetailView({ updateUser }) {
 
     return (
         <>
+        <Container fluid>
+            <Row className="pb-3 d-flex justify-content-between align-items-center">
+                <Col xs={11} />
+
+                <Col xs={1}>
+                    <CartButton />
+                </Col>
+
+            </Row>
+        </Container>
+
         <NavBar_PhotoShoot updateUser={updateUser}/>
         <PhotosContainer />
         </>
