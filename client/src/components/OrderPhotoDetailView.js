00851f33194e1b51cc6e4dc4d2e1a0c4bd76785b
Stuart Forrester <stforrester@gmail.com>
2025-03-05 11:25:03 -0600

    adds OrderPhotoDetailView so that a user can get a close up view of the selected photo in their placed order

--- client/src/components/OrderPhotoDetailView.js
+++ client/src/components/OrderPhotoDetailView.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from "react"
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Button from 'react-bootstrap/Button'
+import { Image } from 'cloudinary-react'
 
-function OrderPhotoDetailView() {
-
+function OrderPhotoDetailView({ photo, handleSetPhoto }) {
+    return (
+        <Container>
+            <Row>
+                <Image cloudName="stuart-forrester" publicId={photo.cloudinary_link} alt={`Photo ${photo.id} Image`} />
+            </Row>
+            <Row>
+                <Col xs={4}>
+                    <Button>Download</Button>
+                </Col>
+                <Col xs={4}>
+                    ${photo.photo_price}
+                </Col>
+                <Col xs={4}>
+                    <Button onClick={()=>handleSetPhoto(null)}>Return to Order</Button>
+                </Col>
+            </Row>
+        </Container>
+    )
 }
 
 export default OrderPhotoDetailView
