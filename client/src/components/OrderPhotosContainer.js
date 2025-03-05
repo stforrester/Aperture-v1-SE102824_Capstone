27ed48fd7e9481458aa9007b715104559cd24618
Stuart Forrester <stforrester@gmail.com>
2025-03-05 11:16:15 -0600

    adds OrderPhotosContainer which performs a fetch to the '/orders/<id:id>' API route to set 'order' state equal to the order selected by the user; this will display the photos in that order and allow the user to view each of these photos in detail

--- client/src/components/OrderPhotosContainer.js
+++ client/src/components/OrderPhotosContainer.js
@@ -1,6 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+
+import OrderPhotosList from './OrderPhotosList'
+import OrderPhotoDetailView from './OrderPhotoDetailView'
 
 function OrderPhotosContainer() {
+
+    const { id } = useParams()
+
+    const [order, setOrder] = useState()
+    const [photo, setPhoto] = useState()
+    const [error, setError] = useState()
+
+    useEffect(() => {
+        fetch(`/orders/${id}`)
+        .then(response => {
+            if(response.ok){
+                response.json()
+                .then(data =>{
+                    setOrder(data)
+                })
+            }
+            else {
+                response.json()
+                .then(error => setError(error))
+            }
+        })
+    }, [])
+
+    const orderPhotos = () => {
+        let orderPhotos = []
+        order.order_items.forEach(obj => {
+            orderPhotos.push(obj.photo)
+        })
+        return orderPhotos
+    }
+
+    const handleSetPhoto = (photo) => {
+        setPhoto(photo)
+    }
+
+    if (!order) return <div>Loading Order Photos...</div>
+
+    if (!photo) return (
+        <div>
+            <h3>Order: {order.id} - Placed: {order.order_date}</h3>
+            <OrderPhotosList photos={orderPhotos()} handleSetPhoto={handleSetPhoto} ></OrderPhotosList>
+        </div>
+    )
+
+    return (
+        <div>
+            <h3>Order: {order.id} - Placed: {order.order_date}</h3>
+            <OrderPhotoDetailView photo={photo} handleSetPhoto={handleSetPhoto} />
+        </div>
+    )
 
 }
 
